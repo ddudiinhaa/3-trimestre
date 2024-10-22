059864647a4f7f8b7cfeab7ad963befee533025e
ddudiinhaa <142030963+ddudiinhaa@users.noreply.github.com>
2024-10-22 11:02:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    //categoria,
+    'Progrmação'
+    //pergunta,
+    'O que é JavaScript'
+   // resposta,
+    'O JavaScript é uma linguagem de programção'
+)
+
+criaCartao(
+    //categoria,
+    'Progrmação'
+    //pergunta,
+    'O que é JavaScript'
+   // resposta,
+    'O JavaScript é uma linguagem de programção'
+)
+
+criaCartao(
+    //categoria,
+    'Progrmação'
+    //pergunta,
+    'O que é JavaScript'
+   // resposta,
+    'O JavaScript é uma linguagem de programção'
+)
