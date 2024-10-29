abdff5c3ea48ffbde3c18fbac4f536f969a60f98
ddudiinhaa <142030963+ddudiinhaa@users.noreply.github.com>
2024-10-29 11:00:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    
+    'Progrmação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programção'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'É Brasilia '
+)
+
+criaCartao(
+    'Progrmação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programção'
+)
+criaCartao(
+    'Literatura',
+    'Qual é o nome da Saga de Arlen Fardos',
+    'O nome é Ciclo Das Trevas'
+)
