7097bc36aae8bda5091ffcccdde96bdbb5d4bd92
ddudiinhaa <142030963+ddudiinhaa@users.noreply.github.com>
2024-11-05 10:43:16 -0300

    app.js

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className='cartao'
-
-
+    cartao.innerHTML='<div class="cartao__conteudo">
+        <h3>${categoria}$</h3>
+    <div class="cartao__conteudo__pergunta">
+        <P>${pergunta}$</p>
+        </div>
+<div class="cartao__conteudo__resposta">
+        <P>${resposta}$</p>
+        </div>
+        </div>
+    '
+        container,appendChild(cartao)
 
 
 
